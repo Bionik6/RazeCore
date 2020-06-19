85d7c5acb667bc1d9d36431a4489fa9e4e4d8a40
Ibrahima Ciss <bionik6@gmail.com>
2020-06-19 20:19:39 +0000

    Added first networking API for retrieving network data

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -4,6 +4,24 @@ extension RazeCore {
 	
 	public class Networking {
 		
+		public class Manager {
+			public init() { }
+			
+			private let session = URLSession.shared
+			
+			public func loadData(from url: URL, completion: @escaping (NetworkResult<Data>)->Void) {
+				let task = session.dataTask(with: url) { (data, response, error) in
+					let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+					completion(result)
+				}
+				task.resume()
+			}
+		}
+		
+	}
 	
+	public enum NetworkResult<Value> {
+		case success(Value)
+		case failure(Error?)
 	}
 }
--- Tests/RazeCoreTests/RazeNetworkingTests.swift
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -1,17 +1,24 @@
-//
-//  RazeNetworkingTests.swift
-//  RazeCoreTests
-//
-//  Created by Ibrahima Ciss on 19/06/20.
-//
-
 import XCTest
+@testable import RazeCore
 
 final class RazeNetworkingTests: XCTestCase {
 	
-	func testExample() throws {
-		// This is an example of a functional test case.
-		// Use XCTAssert and related functions to verify your tests produce the correct results.
+	func test_load_data_call() {
+		let manager = RazeCore.Networking.Manager()
+		let expectation = XCTestExpectation(description: "called for data")
+		guard let url = URL(string: "https://raywernderlich.com") else {
+			return XCTFail("could not load URL properly")
+		}
+		manager.loadData(from: url) { result in
+			expectation.fulfill()
+			switch result {
+				case .success(let data):
+					XCTAssertNotNil(data, "Response data is nil")
+				case .failure(let error):
+					XCTFail(error?.localizedDescription ?? "Error forming error result")
+			}
+		}
+		wait(for: [expectation], timeout: 5.0)
 	}
-	
+
 }
