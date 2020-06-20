c8205dfb8c7e2b3a434ccaa873051dfd45435f76
Ibrahima Ciss <bionik6@gmail.com>
2020-06-20 14:53:38 +0000

    Update Color.swift

--- Sources/RazeCore/Color.swift
+++ Sources/RazeCore/Color.swift
@@ -10,20 +10,19 @@ extension RazeCore {
 		///   - alpha: A number between 0.0 and 1.0 indicating how transparent the color is
 		/// - Returns: A UIColor defined by the `hexString` parameter
 		internal static func fromHexString(_ hexString: String, alpha: CGFloat = 1.0) -> UIColor {
+			let r, g, b: CGFloat
 			let offset = hexString.hasPrefix("#") ? 1 : 0
 			let start = hexString.index(hexString.startIndex, offsetBy: offset)
 			let hexColor = String(hexString[start...])
-			
 			let scanner = Scanner(string: hexColor)
-			scanner.scanLocation = 0
-			var rgbValue: UInt64 = 0
-			scanner.scanHexInt64(&rgbValue)
-			
-			let r = (rgbValue & 0xff0000) >> 16
-			let g = (rgbValue & 0xff00) >> 8
-			let b = rgbValue & 0xff
-			
-			return UIColor(red: CGFloat(r) / 0xff, green: CGFloat(g) / 0xff, blue: CGFloat(b) / 0xff, alpha: alpha)
+			var hexNumber: UInt64 = 0
+			if scanner.scanHexInt64(&hexNumber) {
+				r = CGFloat((hexNumber & 0xff0000) >> 16) / 255
+				g = CGFloat((hexNumber & 0x00ff00) >> 8) / 255
+				b = CGFloat(hexNumber & 0x0000ff) / 255
+				return UIColor(red: r, green: g, blue: b, alpha: alpha)
+			}
+			return UIColor(red: 0, green: 0, blue: 0, alpha: alpha)
 		}
 		
 		/// The most eye-pleasing color know to all humanity
